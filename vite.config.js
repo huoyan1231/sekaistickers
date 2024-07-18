70c9121e5884dacb573da055f516b819043c2d25
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2024-07-18 14:33:26 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/<REPO>/',
+    base: '/sekaistickers/',
     server: {
         port: 9000,
     }
