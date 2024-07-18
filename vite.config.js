9e1ef53dec3e3e3ad9f39881cf28b2c87e22fd5d
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2024-07-18 14:23:01 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,3 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 9000,
-    }
-})
