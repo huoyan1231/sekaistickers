1fe4e5261bab7dba085b5f9e7512aa03c6be6629
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2024-07-18 13:35:09 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react()
+        chunkSplitPlugin()
+        ],
     server: {
         port: 9000,
-    },
-    build: {
-        rollupOptions: {
-            output:{
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
     }
 })
