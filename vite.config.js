65ef0e84237cedb1a94e6a3970897985bd949c6d
huoyan1231 <53113734+huoyan1231@users.noreply.github.com>
2024-07-18 13:50:23 +0800

    3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(),
-        chunkSplitPlugin()
-        ],
+    plugins: [react()],
     server: {
         port: 9000,
     }
